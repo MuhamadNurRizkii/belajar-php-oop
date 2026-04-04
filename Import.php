fd6f84653220c8cb6e2c07d2c5d539602075820e
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-04-04 21:31:58 +0700

    Import

---
+++ Import.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'data/Conflict.php';
+require_once 'data/Helper.php';
+
+use Data\One\Conflict;
+use function Helper\helpMe;
+use const Helper\APPLICATION;
+
+$conflict = new Conflict();
+helpMe();
+echo APPLICATION;
--- data/Helper.php
+++ data/Helper.php
@@ -7,4 +7,9 @@ function helpMe()
     echo "HELP ME" . PHP_EOL;
 }
 
+function sayHello()
+{
+    echo "Hello World" . PHP_EOL;
+}
+
 const APPLICATION = "Belajar PHP OOP";
